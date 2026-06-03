47e7cd0bf305a5113c4935afa6fa3a724bc09cb9
Ayhan Tefik <ayhantefik93@gmail.com>
2026-06-04 00:47:15 +0200

    firebase aws cluster issue

--- server/src/firebase/firebaseAdmin.ts
+++ server/src/firebase/firebaseAdmin.ts
@@ -1,14 +1,24 @@
 import admin from "firebase-admin";
-import { readFile } from "fs/promises";
+// import { readFile } from "fs/promises";
 
-const buffer = await readFile(
-    new URL("../../firebaseServiceAccountKey.json", import.meta.url)
-);
+// const buffer = await readFile(
+//     new URL("../../firebaseServiceAccountKey.json", import.meta.url)
+// );
 
-const serviceAccount = JSON.parse(buffer.toString());
+const FIREBASE_ACCOUNT_KEY = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
+if (!FIREBASE_ACCOUNT_KEY) {
+    throw new Error("CONNECTION_STRING is missing");
+}
+
+const serviceAccount = require(FIREBASE_ACCOUNT_KEY);
+
+admin.credential.cert(serviceAccount)
+
+// const serviceAccount = JSON.parse(buffer.toString());
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+// });
 
 export default admin;
