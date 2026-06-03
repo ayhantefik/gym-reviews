28fd4b8ec4a3b548fce395d877c409951d872fba
Ayhan Tefik <ayhantefik93@gmail.com>
2026-06-03 21:57:40 +0200

    Mongo Atlas added

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,9 @@ import { seedGyms } from "./src/data/seed.js";
 import mongoose from "mongoose";
 import gymRoute from "./src/routes/gyms.ts"
 import reviewRoute from "./src/routes/reviews.ts"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const app = express();
 
@@ -12,7 +15,15 @@ app.use(express.json());
 
 const PORT = 3000;
 
-mongoose.connect("mongodb://localhost:27017/gym-reviews-db");
+const CONNECTION_STRING = process.env.atlas_URL;
+
+console.log("CONNECTION_STRING ", CONNECTION_STRING)  
+
+if (!CONNECTION_STRING) {
+    throw new Error("CONNECTION_STRING is missing");
+}
+
+mongoose.connect(CONNECTION_STRING);
 
 app.use("/gyms", gymRoute);
 app.use("/reviews", reviewRoute);
