4bbbf245d0587225c33a9a706eccc4b31157f3c3
Ayhan Tefik <ayhantefik93@gmail.com>
2026-06-04 16:36:47 +0200

    atlas mongodb aws issue

--- server/src/firebase/firebaseAdmin.ts
+++ server/src/firebase/firebaseAdmin.ts
@@ -1,10 +1,4 @@
 import admin from "firebase-admin";
-import { readFile } from "fs/promises";
-
-const buffer = await readFile(
-    new URL("../../firebaseServiceAccountKey.json", import.meta.url)
-);
-
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
